2f533f0ea03969997a7042ac6a5ea5c0624a34aa
Thomas Vandal <thomas.vandal@umontreal.ca>
2024-08-20 16:52:15 -0400

    Setup ltex_extra.nvim

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,9 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      { "barreiroleo/ltex_extra.nvim" },
+    },
     opts = {
       servers = {
         -- TODO: Move to a python.lua file?
@@ -16,7 +19,24 @@ return {
           }
         },
         ltex = {},
-      }
-    }
+      },
+      setup = {
+        -- See https://github.com/LazyVim/LazyVim/discussions/403
+        ltex = function(_, opts)
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client.name == "ltex" then
+                require("ltex_extra").setup({
+                  load_langs = { "en-CA", "fr" },
+                  init_check = true,
+                  path = vim.fn.stdpath("config") .. "/spell",
+                })
+              end
+            end
+          })
+        end,
+      },
+    },
   }
 }
