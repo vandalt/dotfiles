a9efee7dd53c559780706d61d9ca56f4e19253f9
Thomas Vandal <thomas.vandal@umontreal.ca>
2024-11-15 09:31:23 +0100

    Vim

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -14,3 +14,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank({ timeout = 100 })
   end,
 })
+
+-- https://github.com/LazyVim/LazyVim/issues/4509#issuecomment-2431509475
+vim.api.nvim_create_autocmd("TermOpen", {
+  -- Re-enable some useful terminal bindings overriden by LazyVim/snacks.nvim
+  callback = function(ev)
+    vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = ev.buf, nowait = true })
+    vim.keymap.set("t", "<c-j>", "<c-j>", { buffer = ev.buf, nowait = true })
+    vim.keymap.set("t", "<c-k>", "<c-k>", { buffer = ev.buf, nowait = true })
+    vim.keymap.set("t", "<c-h>", "<c-h>", { buffer = ev.buf, nowait = true })
+    if vim.bo.filetype ~= "snacks_terminal" then
+      vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+    end
+  end,
+})
--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    style = "night",
-    transparent = false, -- Will be transparent only if match terminal theme
-    styles = {
-      -- These can be dark, transparent or normal
-      sidebars = "dark",
-      floats = "normal",
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      transparent = false, -- Will be transparent only if match terminal theme
+      styles = {
+        -- These can be dark, transparent or normal
+        sidebars = "dark",
+        floats = "normal",
+      },
     },
   },
   {
@@ -14,6 +16,6 @@ return {
     opts = {
       -- Catppuccin and Tokyonight installed with LazyVim
       colorscheme = "tokyonight",
-    }
-  }
+    },
+  },
 }
