4ff2b1f256e6c56674ff0c1e5a23965589bdce9e
Thomas Vandal <thomas.vandal@umontreal.ca>
2024-12-23 18:31:01 -0500

    Use `langs.lua` instead of per-language files

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -9,18 +9,6 @@ return {
         virtual_text = false,
       },
       servers = {
-        -- TODO: Move to a python.lua file?
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                -- NOTE: Pyright uses single file diagnosticmode
-                -- which means fidget will not be shown by default
-                typeCheckingMode = "off",
-              }
-            }
-          }
-        },
         ltex = {
           settings = {
             ltex = {
--- private_dot_config/nvim/lua/plugins/python.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "linux-cultist/venv-selector.nvim",
-    opts = {
-      anaconda_base_path = os.getenv("HOME") .. "./miniforge3",
-      anaconda_envs_path = os.getenv("HOME") .. "./miniforge3/envs",
-    },
-  },
-}
