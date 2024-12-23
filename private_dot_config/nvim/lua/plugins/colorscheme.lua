3b79ceff9fed6486eb4f823e9ea4c4e2a1c0538a
Thomas Vandal <thomas.vandal@umontreal.ca>
2024-12-23 18:30:33 -0500

    Brighter line numbers in tokyonight

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,12 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      style = "night",
+      style = "moon",
       transparent = false, -- Will be transparent only if match terminal theme
+      on_colors = function(colors)
+        -- Make line numbers a bit brighter
+        colors.fg_gutter = colors.comment
+      end,
       styles = {
         -- These can be dark, transparent or normal
         sidebars = "dark",
