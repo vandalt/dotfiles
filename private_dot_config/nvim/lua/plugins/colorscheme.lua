5db7a4bfe49515225c5fd5574c127ae9042295b9
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-02-11 11:11:09 -0500

    kanagawa

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,22 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight-night]])
-    end,
+    lazy = true,
   },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false, -- Load main colorscheme during startup...
+    priority = 1000, -- before all other start plugins
+    opts = {
+      background = {
+        dark = "wave",
+        light = "lotus",
+      }
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      -- load the colorscheme here
+      vim.cmd([[colorscheme kanagawa]])
+    end,
+  }
 }
