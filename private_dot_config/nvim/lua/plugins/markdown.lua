31f180fbb02838f104a177935914ab76d77290f4
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-07-28 14:54:30 -0400

    Remove image.nvim

--- private_dot_config/nvim/lua/plugins/markdown.lua
+++ private_dot_config/nvim/lua/plugins/markdown.lua
@@ -157,20 +157,6 @@ return {
     },
   },
   {
-    "3rd/image.nvim",
-    enabled = false,
-    ft = { "markdown" },
-    opts = {
-      backend = "kitty", -- whatever backend you would like to use
-      max_width = 100,
-      max_height = 20,
-      max_height_window_percentage = math.huge,
-      max_width_window_percentage = math.huge,
-      window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-    },
-  },
-  {
     "HakonHarnes/img-clip.nvim",
     cmd = { "PasteImage", "ImgClipDebug", "ImgClipConfig" },
     opts = {
