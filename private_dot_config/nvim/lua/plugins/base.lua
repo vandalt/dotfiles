1d5340f5ce22c86b3047fda7ff974630c1321ef9
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-07-28 14:52:19 -0400

    Move toggleterm to editor.lua

--- private_dot_config/nvim/lua/plugins/base.lua
+++ private_dot_config/nvim/lua/plugins/base.lua
@@ -4,31 +4,6 @@ return {
   "justinmk/vim-gtfo", -- Exit vim, keeping for gof, not using got
   { "nvim-lua/plenary.nvim", lazy = true },
   {
-    "akinsho/toggleterm.nvim",
-    dev = true,
-    opts = {
-      shade_terminals = true,
-      shading_factor = "-30",
-      shading_ratio = "-0.3",
-      open_mapping = [[<c-/>]],
-      size = function(term)
-        if term.direction == "horizontal" then
-          return vim.o.lines * 0.35
-        elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
-        end
-      end,
-    },
-    keys = {
-      { "<c-/>", desc = "Toggle Terminal" }, -- this key is useful to load plugin, not just desc
-      { "<c-s-/>", "<Cmd>ToggleTerm direction=vertical<CR>", desc = "Toggle vertical terminal" },
-      -- See source to setup with args if need it
-      { "<leader>ip", "<Cmd>TermExec cmd='ipython' go_back=0<CR>", desc = "Terminal with IPython" },
-      { "<leader>rp", "<Cmd>TermExec cmd='python %'<CR>", desc = "Run Python file" },
-      { "<leader>rt", "<Cmd>TermExec cmd='pytest'<CR>", desc = "Run Pytest" },
-    },
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000, -- before all other start plugins
