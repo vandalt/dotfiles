7d2fae5b09dbaf5b487cb1155dc8ec6466edfd30
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-11-06 14:24:39 -0500

    tmux workaround for nvim background

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -4,5 +4,12 @@ vim.g.snacks_animate = false
 vim.g.deprecation_warnings = true
 vim.g.lazyvim_python_lsp = "basedpyright"
 
+-- Workaround to set background automatically in tmux
+-- Ref: https://github.com/neovim/neovim/issues/17070
+if vim.loop.os_gethostname() ~= "yoga" then
+  ---@diagnostic disable-next-line: param-type-mismatch
+  vim.loop.fs_write(2, "\27Ptmux;\27\27]11;?\7\27\\", -1, nil)
+end
+
 vim.opt.spelllang = { "fr", "en" }
 vim.opt.title = true
