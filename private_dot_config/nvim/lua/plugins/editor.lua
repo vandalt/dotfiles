5f5043ac9be6d172c910835b6cb0ad11842d2de3
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-11-10 12:56:30 -0500

    oil.nvim

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,33 @@
 return {
   {
+    "stevearc/oil.nvim",
+    enabled = true,
+    lazy = false,
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
+        ["<C-a>"] = { "actions.select", opts = { horizontal = true } },
+      },
+    },
+    keys = {
+      { "-", "<Cmd>Oil<CR>", desc = "Open parent directory" },
+    },
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        replace_netrw = false,
+      },
+    },
+  },
+  {
     "folke/which-key.nvim",
     opts = {
       spec = {
