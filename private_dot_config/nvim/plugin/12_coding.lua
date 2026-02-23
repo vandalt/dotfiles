33a20023c38521b14869d400f2cd2600f19f4e01
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-02-23 13:50:15 -0500

    Update foldexpr to built-in one

--- private_dot_config/nvim/plugin/12_coding.lua
+++ private_dot_config/nvim/plugin/12_coding.lua
@@ -113,8 +113,8 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = all_filetypes,
   callback = function()
     vim.treesitter.start()
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo.foldmethod = "expr"
-    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
