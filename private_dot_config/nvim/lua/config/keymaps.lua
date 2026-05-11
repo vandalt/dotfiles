396f707f9ab296841c9e27c1f7c65577a733a29b
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-05-11 10:44:02 -0400

    Mappings to clear whitespaces and remove swapfiles

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -13,6 +13,25 @@ map("t", "<Esc><Esc>", "<C-\\><C-n>", "Normal mode (terminal)")
 -- Undotree
 map("n", "<leader>uu", "<Cmd>Undotree<CR>", "Toggle Undotree")
 
+-- Clear white spaces
+map("n", "<leader>cw", function()
+  local view = vim.fn.winsaveview()
+  vim.cmd("%s/\\s\\+$//e")
+  vim.fn.winrestview(view)
+end, "Clear whitespace")
+
+-- Remove swap file
+map("n", "<leader>rs", function()
+  local swapfile = vim.fn.swapname(vim.fn.bufnr())
+  swapfile = swapfile:gsub("%.swo$", ".swp")
+  if swapfile and swapfile ~= "" then
+    vim.fn.delete(swapfile)
+    vim.notify("Removed swap file: " .. swapfile, vim.log.levels.INFO)
+  else
+    vim.notify("No swap file found", vim.log.levels.WARN)
+  end
+end, "Remove swap file")
+
 -- Jupyter snippets
 map("n", "<leader>jn", "icodeblock<C-j>python<C-l>", "New cell", { remap = true })
 map("i", "<C-CR>", "codeblock<C-j>python<C-l>", "New cell", { remap = true })
